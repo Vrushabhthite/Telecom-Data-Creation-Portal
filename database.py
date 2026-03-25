37afd7b1b59e1e18d368e1e1cad262295dee1208
Vrushabh Thite <rishibhthite9322@gmail.com>
2026-03-25 18:49:15 +0530

    fix postgres ssl issue

--- database.py
+++ database.py
@@ -4,9 +4,12 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is missing")
+
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"sslmode": "require"}
+    pool_pre_ping=True   # IMPORTANT (Render fix)
 )
 
 SessionLocal = sessionmaker(bind=engine)
