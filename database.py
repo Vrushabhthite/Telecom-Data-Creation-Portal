06d7d93770b794ff7c351a9bd725ebeb89e88374
Vrushabh Thite <rishibhthite9322@gmail.com>
2026-03-25 17:43:16 +0530

    fills modified

--- database.py
+++ database.py
@@ -1,25 +1,4 @@
 
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, declarative_base
-
-
-# DATABASE_URL = "mysql+pymysql://root:Coder%40123@localhost/smart_disk_automation"
-
-# engine = create_engine(DATABASE_URL)
-
-# SessionLocal = sessionmaker(bind=engine)
-# Base = declarative_base()
-
-
-# import os
-# from sqlalchemy import create_engine
-
-# DATABASE_URL = "postgresql://ioms_user:6xntcph7oZ2cpFWztGsqiyRD0EW7LNgR@dpg-d71si1i4d50c73bvc1k0-a.singapore-postgres.render.com/ioms_data"
-
-# engine = create_engine(
-#     DATABASE_URL,
-#     connect_args={"sslmode": "require"}  # VERY IMPORTANT
-    
 import os
 from sqlalchemy import create_engine
 
